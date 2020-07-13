0533915289636d8090d606186111a669d4a68e77
Aaron Hauth <aaronhauth@gmail.com>
2020-07-12 23:41:34 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World'));
+app.get('/cat-facts/', (req, res) => {
+    https.get('https://cat-fact.herokuapp.com/facts/random/', (resp) => {
+        let data = '';
+        resp.on('data', chunk => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            const json = JSON.parse(data)
+            res.send(json.text);
+        })
+    });
+
+}); 
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+function processCatFacts(req, res) {
+
+}
